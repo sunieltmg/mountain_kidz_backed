695001d66529701cc175d69a03a01e164447a038
sunieltmg <technicalnepal31@gmail.com>
2022-06-17 22:41:55 +0545

    connect to mongoDB atlas

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 import express from 'express';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
 
 const app = express();
+
+//middleware
+app.use(morgan('dev'));
+
+const dbUrl =
+  'mongodb+srv://mountain_kidz:mountain_kidz@cluster0.p6jj9.mongodb.net/?retryWrites=true&w=majority';
+const dbOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(dbUrl, dbOptions, () => {
+  console.log('connected to db');
+});
 
 // routes
 app.get('/', (req, res) => {
