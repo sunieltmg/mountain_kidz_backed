3380dee39fa5445a9473a9793c0ef412e11c93da
sunieltmg <technicalnepal31@gmail.com>
2022-07-16 17:41:36 +0545

    add redis for caching api response

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import 'dotenv/config';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import { default as courseRoutes } from './routes/api/courseRoute.js';
 import { default as registerRoutes } from './routes/registerRoute.js';
 import { default as authRoutes } from './routes/authRoute.js';
@@ -13,9 +15,34 @@ import { default as logOutRoutes } from './routes/logOutRoute.js';
 import connectDB from './config/dbConn.js';
 import verifyJWT from './middleware/verifyJWT.js';
 
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Mountain Kidz API',
+      version: '1.0.0',
+      description: 'Simple api for school management system.',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Internal staging server for testing',
+      },
+      {
+        url: 'https://mountain-kidz.herokuapp.com/',
+        description: 'Main (production) server',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'],
+};
+
+const swaggerJSSpec = swaggerJsdoc(swaggerOptions);
+
 const app = express();
 
 //middleware
+app.use('/apiDocs', swaggerUi.serve, swaggerUi.setup(swaggerJSSpec));
 app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
--- routes/api/courseRoute.js
+++ routes/api/courseRoute.js
@@ -8,6 +8,37 @@ import {
   deleteSingleCourse,
 } from '../../controllers/courseController.js';
 const router = Router();
+// creating swagger models
+
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *   Course:
+ *    type:Object
+ *    required:
+ *     - title
+ *     - description
+ *     - image
+ *    properties:
+ *     title:
+ *      type:String
+ *      description:"Title of the course"
+ *     description:
+ *      type:String
+ *      description:"Descrition of the course"
+ *     image:
+ *      type:String
+ *      description:"Image of the course"
+ *    example:
+ *     title:"English"
+ *     description:"English is the internationally spoken language"
+ *     image:"course image url"
+ *
+ *
+ *
+ */
+
 router.route('/').get(getAllCourses).post(createNewCourse).delete(deleteCourse);
 
 router
