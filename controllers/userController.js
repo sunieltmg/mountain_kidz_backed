a68bb09338dda6b51057f2094c492753b4c62c3c
sunieltmg <technicalnepal31@gmail.com>
2022-08-22 19:14:14 +0545

    add youtube video link

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,3 +4,8 @@ export const deleteAllUser = async (req, res) => {
   const response = userModel.deleteMany();
   return res.status(200).json({ message: 'User model cleared successfully' });
 };
+
+export const getSingleUser = async (req, res) => {
+  const response = userModel.findOne();
+  return res.status(200).json({ message: response.body });
+};
